e29f26eca3e44f0ed06ea198666f6de9723806f9
wright-ian <i.f.wright@gmail.com>
2023-02-10 23:29:24 -0500

    Added main menu, created add method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int menuSelection = input.nextInt();
+        System.out.println("Select one of the following equations. To select the equation, enter the corresponding number.");
+        System.out.println("1. Add two numbers");
+        System.out.println("2. Subtract two numbers");
+        System.out.println("3. Multiply two numbers");
+        System.out.println("4. Divide two numbers");
+        System.out.println("5. Square a number");
+        System.out.println("6. Find the square root of a number");
 
+        switch (menuSelection) {
+            case 1:
+        }
+    }
+    public static double add(double a, double b) {
+        return a + b;
     }
 }
+
+
